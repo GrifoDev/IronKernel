f7c7a4f3ca436be7045cf927c0cab95130458137
Tkkg1994 <luca@grifodev.ch>
2018-01-17 00:50:29 +0100

    ramdisk: BQL6 update + AIK V3.1

--- build.sh
+++ build.sh
@@ -193,7 +193,7 @@ FUNC_BUILD_RAMDISK()
 			mv -f $RDIR/arch/$ARCH/boot/boot.img-zImage $RDIR/ramdisk/G950F/split_img/boot.img-zImage
 			mv -f $RDIR/arch/$ARCH/boot/boot.img-dtb $RDIR/ramdisk/G950F/split_img/boot.img-dtb
 			cd $RDIR/ramdisk/G950F
-			./repackimg.sh
+			./repackimg.sh --nosudo
 			echo SEANDROIDENFORCE >> image-new.img
 			;;
 		*)
@@ -210,7 +210,7 @@ FUNC_BUILD_RAMDISK()
 			mv -f $RDIR/arch/$ARCH/boot/boot.img-zImage $RDIR/ramdisk/G955F/split_img/boot.img-zImage
 			mv -f $RDIR/arch/$ARCH/boot/boot.img-dtb $RDIR/ramdisk/G955F/split_img/boot.img-dtb
 			cd $RDIR/ramdisk/G955F
-			./repackimg.sh
+			./repackimg.sh --nosudo
 			echo SEANDROIDENFORCE >> image-new.img
 			;;
 		*)
@@ -227,7 +227,7 @@ FUNC_BUILD_RAMDISK()
 			mv -f $RDIR/arch/$ARCH/boot/boot.img-zImage $RDIR/ramdisk/N950F/split_img/boot.img-zImage
 			mv -f $RDIR/arch/$ARCH/boot/boot.img-dtb $RDIR/ramdisk/N950F/split_img/boot.img-dtb
 			cd $RDIR/ramdisk/N950F
-			./repackimg.sh
+			./repackimg.sh --nosudo
 			echo SEANDROIDENFORCE >> image-new.img
 			;;
 		*)
